37bf8c00e4079885028a2bb255f6d1fb5da612bd
smartgoo <cvallone28@gmail.com>
2025-12-21 08:14:05 -0500

    DerivationPath move to newtype pattern and cleanup

--- src/wallet/keys/derivation.rs
+++ src/wallet/keys/derivation.rs
@@ -4,9 +4,7 @@ use std::str::FromStr;
 
 #[pyclass(name = "DerivationPath")]
 #[derive(Clone)]
-pub struct PyDerivationPath {
-    inner: kaspa_bip32::DerivationPath,
-}
+pub struct PyDerivationPath(kaspa_bip32::DerivationPath);
 
 #[pymethods]
 impl PyDerivationPath {
@@ -14,41 +12,38 @@ impl PyDerivationPath {
     pub fn new(path: &str) -> PyResult<PyDerivationPath> {
         let inner = kaspa_bip32::DerivationPath::from_str(path)
             .map_err(|err| PyException::new_err(err.to_string()))?;
-        Ok(Self { inner })
+        Ok(PyDerivationPath(inner))
     }
 
-    #[pyo3(name = "is_empty")]
     pub fn is_empty(&self) -> bool {
-        self.inner.is_empty()
+        self.0.is_empty()
     }
 
     #[pyo3(name = "length")]
     pub fn len(&self) -> usize {
-        self.inner.len()
+        self.0.len()
     }
 
-    #[pyo3(name = "parent")]
     pub fn parent(&self) -> Option<PyDerivationPath> {
-        self.inner.parent().map(|inner| Self { inner })
+        self.0.parent().map(|inner| PyDerivationPath(inner))
     }
 
-    #[pyo3(name = "push")]
     #[pyo3(signature = (child_number, hardened=None))]
     pub fn push(&mut self, child_number: u32, hardened: Option<bool>) -> PyResult<()> {
         let child = ChildNumber::new(child_number, hardened.unwrap_or(false))
             .map_err(|err| PyException::new_err(err.to_string()))?;
-        self.inner.push(child);
+        self.0.push(child);
         Ok(())
     }
 
     #[pyo3(name = "to_string")]
     pub fn to_str(&self) -> String {
-        self.inner.to_string()
+        self.0.to_string()
     }
 }
 
 impl From<PyDerivationPath> for kaspa_bip32::DerivationPath {
     fn from(value: PyDerivationPath) -> Self {
-        value.inner
+        value.0
     }
 }

e655a35aaa958a811ce1cc944cb77beb9bd86482
smartgoo <cvallone28@gmail.com>
2026-01-10 09:32:50 -0500

    docs: cleanup kaspa_rpc.pyi stub file

--- kaspa_rpc.pyi
+++ kaspa_rpc.pyi
@@ -1,15 +1,9 @@
 """
-TypedDict definitions for Kaspa RPC request and response types.
+TypedDict definitions for Kaspa RPC request/response messages & contained types.
 
-These types describe the dictionary structures used when calling RPC methods
-on the RpcClient. Request types are passed as the `request` parameter, and
-response types describe what the async methods return.
+This file is maunally maintained and appended to kaspa.pyi file via stub gen process.
 
-Example:
-    >>> client = RpcClient(...)
-    >>> await client.connect()
-    >>> response: GetBlockDagInfoResponse = await client.get_block_dag_info()
-    >>> print(response["blockCount"])
+Long term, attempts should be made to auto generate.
 """
 
 from enum import Enum
@@ -81,6 +75,7 @@ class RpcTransactionInput(TypedDict):
 
 
 class RpcTransactionOutputVerboseData(TypedDict):
+    """Verbose data for a transaction output."""
     scriptPublicKeyType: str
     scriptPublicKeyAddress: str
 
@@ -153,6 +148,7 @@ class RpcBlock(TypedDict):
 
 
 class RpcRawHeader(TypedDict):
+    """A raw block header without a cached hash."""
     version: int
     parentsByLevel: list[list[str]]
     hashMerkleRoot: str
@@ -160,7 +156,7 @@ class RpcRawHeader(TypedDict):
     utxoCommitment: str
     timestamp: int
     bits: int
-    nonce: u64
+    nonce: int
     daaScore: int
     blueWork: str
     blueScore: int
@@ -386,19 +382,6 @@ class RpcOptionalTransactionInput(TypedDict):
     sequence: int | None
     sigOpCount: int | None
     verboseData: RpcOptionalTransactionInputVerboseData | None # TODO
-
-
-class RpcOptionalTransactionOutput(TypedDict):
-    """Represents a transaction output with optional fields based on verbosity level.
-
-    Attributes:
-        value: The output value in sompi. Level: Low.
-        scriptPublicKey: The script public key for this output. Level: Low.
-        verboseData: Additional verbose data for this output.
-    """
-    value: int | None
-    scriptPublicKey: RpcScriptPublicKey | None
-    verboseData: RpcOptionalTransactionOutputVerboseData | None # TODO
 
 
 class RpcOptionalTransactionVerboseData(TypedDict):
@@ -740,6 +723,7 @@ class GetPeerAddressesResponse(TypedDict):
 
 
 class BandwidthMetrics(TypedDict):
+    """Bandwidth metrics for various protocols."""
     borshBytesTx: int
     borshBytesRx: int
     jsonBytesTx: int
@@ -762,6 +746,7 @@ class GetMetricsResponse(TypedDict, total=False):
 
 
 class ConnectionsProfileData(TypedDict):
+    """Profile data for connection resource usage."""
     cpuUsage: int
     memoryUsage: int
 
